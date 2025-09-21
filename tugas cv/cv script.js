af1cd882fa590cad387e29bcd06b09cf2c38614f
MunirotunNimah123 <munirotunnikmah014@gmail.com>
2025-09-22 01:30:12 +0800

    up

--- tugas cv/cv script.js
+++ tugas cv/cv script.js
@@ -1,3 +1,3 @@
 function hubungiSaya() {
-    alert("Terima kasih! Silakan kirim email ke munirotunnimah014@example.com");
+    alert("Terima kasih! Silakan kirim email ke munirotunnimah014@gmail.com");
 }
